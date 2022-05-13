65abf31f15d6d004ae15ccae4e329d3723340ef7
Andy Stewart <lazycat.manatee@gmail.com>
2022-05-13 20:53:44 +0800

    Add encoding for get_command_result.

--- core/utils.py
+++ core/utils.py
@@ -106,7 +106,8 @@ def get_emacs_func_result(method_name, args):
 def get_command_result(command_string, cwd):
     import subprocess
     process = subprocess.Popen(command_string, cwd=cwd, shell=True, text=True,
-                               stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                               stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+                               encoding="utf-8")
     ret = process.wait()
     return "".join((process.stdout if ret == 0 else process.stderr).readlines()).strip()
 
