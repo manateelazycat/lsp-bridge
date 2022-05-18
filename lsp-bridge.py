48ce5aeb31061172659407832156bfae743b310c
Andy Stewart <lazycat.manatee@gmail.com>
2022-05-18 18:53:51 +0800

    Try to fix issue #106

--- lsp-bridge.py
+++ lsp-bridge.py
@@ -136,9 +136,9 @@ class LspBridge:
 
     def _open_file(self, filepath: Path):
         project_path: Path = get_project_path(filepath)
-        lang_server: str = get_emacs_func_result("get-lang-server", project_path, filepath)
+        lang_server: str = get_emacs_func_result("get-lang-server", project_path.as_path(), filepath)
         lang_server_info: dict = load_lang_server_info(lang_server)
-        lsp_server_name = "{}#{}".format(project_path, lang_server_info["name"])
+        lsp_server_name = "{}#{}".format(project_path.as_path(), lang_server_info["name"])
 
         if lsp_server_name not in self.lsp_server_dict:
             self.lsp_server_dict[lsp_server_name] = LspServer(
