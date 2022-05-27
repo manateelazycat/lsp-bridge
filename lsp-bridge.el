636eeaff7bca96d7b8172ef65bbafb50728064c0
yqu212 <yqu212@gmail.com>
2022-05-27 21:15:20 +0800

    Support org source code block

--- lsp-bridge.el
+++ lsp-bridge.el
@@ -973,6 +973,11 @@ If optional MARKER, return a marker instead"
 
 (defvar lsp-bridge-mode-map (make-sparse-keymap))
 
+(defcustom lsp-bridge-org-babel-lang-list '("clojure" "latex" "python")
+  "A list of org babel languages in which source code block lsp-bridge will be enabled."
+  :type 'list
+  :group 'lsp-bridge)
+
 ;;;###autoload
 (define-minor-mode lsp-bridge-mode
   "LSP Bridge mode."
@@ -1066,6 +1071,35 @@ If optional MARKER, return a marker instead"
 (defun lsp-bridge--corfu-hide-advisor (&rest args)
   (lsp-bridge-call-async "completion_hide" lsp-bridge-filepath))
 (advice-add #'corfu--popup-hide :after #'lsp-bridge--corfu-hide-advisor)
+
+;; https://tecosaur.github.io/emacs-config/config.html#lsp-support-src
+(cl-defmacro lsp-org-babel-enable (lang)
+  "Support LANG in org source code block."
+  (cl-check-type lang stringp)
+  (let* ((edit-pre (intern (format "org-babel-edit-prep:%s" lang)))
+	 (intern-pre (intern (format "lsp--%s" (symbol-name edit-pre)))))
+    `(progn
+       (defun ,intern-pre (info)
+	 (let ((file-name (->> info caddr (alist-get :file))))
+	   (unless file-name
+	     (setq file-name (make-temp-file "babel-lsp-")))
+	   (setq buffer-file-name file-name)
+	   (lsp-bridge-mode 1)))
+       (put ',intern-pre 'function-documentation
+	    (format "Enable lsp-bridge-mode in the buffer of org source block (%s)."
+		    (upcase ,lang)))
+       (if (fboundp ',edit-pre)
+	   (advice-add ',edit-pre :after ',intern-pre)
+	 (progn
+	   (defun ,edit-pre (info)
+	     (,intern-pre info))
+	   (put ',edit-pre 'function-documentation
+		(format "Prepare local buffer environment for org source block (%s)."
+			(upcase ,lang))))))))
+
+(with-eval-after-load 'org
+  (dolist (lang lsp-bridge-org-babel-lang-list)
+    (eval `(lsp-org-babel-enable ,lang))))
 
 (provide 'lsp-bridge)
 
