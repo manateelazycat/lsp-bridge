08464dc92ea6a80a84ff68716a9df823734eea8f
50ways2sayhard <gjtzone@hotmail.com>
2022-05-31 10:50:30 +0800

    Remove trailing whitespace

--- lsp-bridge.el
+++ lsp-bridge.el
@@ -166,7 +166,6 @@ Setting this to nil or 0 will turn off the indicator."
   :type 'boolean
   :group 'lsp-bridge)
 
-
 (defcustom lsp-bridge-enable-diagnostics t
   "Whether to enable diagnostics."
   :type 'boolean
