02a092cc6b94546af46163aaab0d60e3f81bf791
Andy Stewart <lazycat.manatee@gmail.com>
2022-06-06 15:52:37 +0800

    Use string-match-p instead string-prefix-p

--- acm/acm.el
+++ acm/acm.el
@@ -540,7 +540,7 @@ influence of C1 on the result."
            (lambda (k v)
              (let ((candidate-label (plist-get v :label)))
                (when (or (string-equal keyword "")
-                         (string-prefix-p keyword candidate-label))
+                         (string-match-p (regexp-quote (downcase keyword)) (downcase candidate-label)))
                  (if (> (length candidate-label) acm-menu-candidate-limit)
                      (plist-put v :display-label (format "%s ..." (substring candidate-label 0 acm-menu-candidate-limit)))
                    (plist-put v :display-label candidate-label))
