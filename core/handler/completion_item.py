b58685ecd236a676fd1ebf75c4b375912aa0d738
Andy Stewart <lazycat.manatee@gmail.com>
2022-06-09 10:54:37 +0800

    Fix documentation parse issue and hide doc frame if documentation is None.

--- core/handler/completion_item.py
+++ core/handler/completion_item.py
@@ -17,8 +17,15 @@ class CompletionItem(Handler):
     def process_response(self, response: dict) -> None:
         if response is not None and "documentation" in response:
             response_doc = response["documentation"]
+            if type(response_doc) == dict:
+                if "value" in response_doc:
+                    response_doc = response_doc["value"]
+                else:
+                    response_doc = ""
             
             self.file_action.completion_item_update(
                 self.item_key, 
-                response_doc["value"] if response_doc and "value" in response_doc else "",
+                response_doc.strip(),
                 response["additionalTextEdits"] if "additionalTextEdits" in response else [])
+        else:
+            eval_in_emacs("acm-doc-hide")
