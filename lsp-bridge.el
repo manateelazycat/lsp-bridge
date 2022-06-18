29eae3b656be30d86ef0ff9ef2e240a84a9643d1
ctrl2wei <35016457+ctrl2wei@users.noreply.github.com>
2022-06-18 08:38:02 +0800

    fix typo (#227)
    the type argument of cl-check-type should be string, not stringp

--- lsp-bridge.el
+++ lsp-bridge.el
@@ -1351,7 +1351,7 @@ Auto completion is only performed if the tick did not change."
 ;; https://tecosaur.github.io/emacs-config/config.html#lsp-support-src
 (cl-defmacro lsp-org-babel-enable (lang)
   "Support LANG in org source code block."
-  (cl-check-type lang stringp)
+  (cl-check-type lang string)
   (let* ((edit-pre (intern (format "org-babel-edit-prep:%s" lang)))
          (intern-pre (intern (format "lsp--%s" (symbol-name edit-pre)))))
     `(progn
