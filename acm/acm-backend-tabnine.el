95064eec06863b40943818d2e8dc8b1728744bfa
happyo <xiaxiongzhi@corp.netease.com>
2022-08-23 19:23:10 +0800

    Remove test code

--- acm/acm-backend-tabnine.el
+++ acm/acm-backend-tabnine.el
@@ -1,4 +1,4 @@
-;;; acm-backend-tabnine.el --- Insert description here -*- lexical-binding: t -*-
+;;; acm-backend-tabnine.el --- acm tabnine support -*- lexical-binding: t -*-
 ;;; Commentary:
 ;;; Code:
 
@@ -14,11 +14,9 @@
               tabnine-bridge--disabled)
     (tabnine-bridge-query))
   (let* ((candidates (list))
-         (test-completes '("test1" "test2" "test3"))
          (bounds (bounds-of-thing-at-point 'symbol))
          (thing (thing-at-point 'symbol))
          (tabcandidates (tabnine-bridge--candidates thing)))
-    ;; (debug)
     (setq-local tabnine-bridge--begin-pos (or (car bounds) (point)))
     (when (>= (length keyword) acm-backend-tabnine-min-length)
            (dolist (candidate tabcandidates)
@@ -34,33 +32,14 @@
                        ))
            ))
     candidates))
-       ;; (let ((item (cl-find candidate (funcall get-candidates) :test #'string=)))
-       ;;   (tabnine-bridge--post-completion item)
-;;   )
-;; (defun tabnine-bridge--post-completion (candidate)
-;;   "Replace old suffix with new suffix for CANDIDATE."
-;;   )
-
-  ;; (if (acm-backend-search-words-is-elisp-mode)
-  ;;     (delete-region (car (bounds-of-thing-at-point 'symbol)) (point))
-  ;;   (delete-region
-  ;;    (save-excursion
-  ;;      (skip-syntax-backward "^ " (line-beginning-position))
-  ;;      (point))
-  ;;    (point)))
-  ;; (insert (plist-get candidate-info :label)))
-
-(defun test-tabnine ()
-  (interactive)
-  (message "show tabnine")
-  (message (format "%s" (acm-backend-tabnine-candidates "test"))))
 
 (defun acm-backend-tabnine-candidate-expand (candidate-info bound-start)
   (when tabnine-bridge-auto-balance
     (let ((label (plist-get candidate-info :label)))
       (delete-region bound-start (point))
       (insert label)))
-)
+  )
+
 (provide 'acm-backend-tabnine)
 ;;; acm-backend-tabnine.el ends here
 
