bfc6e9243fcb19f581958c4117c77820055dff83
wcq062821 <wcq-062821@163.com>
2022-09-04 06:26:03 +0800

    citre-backend add mode keyword completion support (#340)
    * add citre support
    
    * citre backend ok
    
    * default disable acm-backend-citre
    
    * add some icons
    
    * update icons
    
    * update icons
    
    * fix stuck when type .
    
    * make code beautiful
    
    * fix enable completion after acm-complete in lsp-bridge-monitor-post-command
    
    * fix some Bug
    
    * citre-backend add mode keyword completion support

--- acm/acm-backend-citre.el
+++ acm/acm-backend-citre.el
@@ -81,15 +81,85 @@
   "Popup citre completions when this option is turn on."
   :type 'boolean)
 
+(defcustom acm-enable-keyword-complete t
+  "push mode keyword to candidate"
+  :type 'boolean)
 
 (defvar-local acm-citre-search-keyword "")
 
 (defvar-local acm-citre-prefix-keyword ""
   "when type . keyword is \"\" so we use symbol befoe . as keyword prefix")
 
+(defvar acm-keywords-alist
+  `((verilog-mode
+     ;; compiler directives, from IEEE 1800-2012 section 22.1
+     "`__FILE__" "`__LINE" "`begin_keywords" "`celldefine" "`default_nettype"
+     "`define" "`else" "`elsif" "`end_keywords" "`endcelldefine" "`endif"
+     "`ifdef" "`ifndef" "`include" "`line" "`nounconnected_drive" "`pragma"
+     "`resetall" "`timescale" "`unconnected_drive" "`undef" "`undefineall"
+     ;; compiler directives not covered by IEEE 1800
+     "`case" "`default" "`endfor" "`endprotect" "`endswitch" "`endwhile" "`for"
+     "`format" "`if" "`let" "`protect" "`switch" "`time_scale" "`uselib"
+     "`while" "after" "alias" "always" "always_comb" "always_ff" "always_latch" "analog" "and"
+     "assert" "assign" "assume" "automatic" "before" "begin" "bind"
+     "bins" "binsof" "bit" "break" "buf" "bufif0" "bufif1" "byte"
+     "case" "casex" "casez" "cell" "chandle" "class" "clocking" "cmos"
+     "config" "const" "constraint" "context" "continue" "cover"
+     "covergroup" "coverpoint" "cross" "deassign" "default" "defparam"
+     "design" "disable" "dist" "do" "edge" "else" "end" "endcase"
+     "endclass" "endclocking" "endconfig" "endfunction" "endgenerate"
+     "endgroup" "endinterface" "endmodule" "endpackage" "endprimitive"
+     "endprogram" "endproperty" "endspecify" "endsequence" "endtable"
+     "endtask" "enum" "event" "expect" "export" "extends" "extern"
+     "final" "first_match" "for" "force" "foreach" "forever" "fork"
+     "forkjoin" "function" "generate" "genvar" "highz0" "highz1" "if"
+     "iff" "ifnone" "ignore_bins" "illegal_bins" "import" "incdir"
+     "include" "initial" "inout" "input" "inside" "instance" "int"
+     "integer" "interface" "intersect" "join" "join_any" "join_none"
+     "large" "liblist" "library" "local" "localparam" "logic"
+     "longint" "macromodule" "mailbox" "matches" "medium" "modport" "module"
+     "nand" "negedge" "new" "nmos" "nor" "noshowcancelled" "not"
+     "notif0" "notif1" "null" "or" "output" "package" "packed"
+     "parameter" "pmos" "posedge" "primitive" "priority" "program"
+     "property" "protected" "pull0" "pull1" "pulldown" "pullup"
+     "pulsestyle_onevent" "pulsestyle_ondetect" "pure" "rand" "randc"
+     "randcase" "randsequence" "rcmos" "real" "realtime" "ref" "reg"
+     "release" "repeat" "return" "rnmos" "rpmos" "rtran" "rtranif0"
+     "rtranif1" "scalared" "semaphore" "sequence" "shortint" "shortreal"
+     "showcancelled" "signed" "small" "solve" "specify" "specparam"
+     "static" "string" "strong0" "strong1" "struct" "super" "supply0"
+     "supply1" "table" "tagged" "task" "this" "throughout" "time"
+     "timeprecision" "timeunit" "tran" "tranif0" "tranif1" "tri"
+     "tri0" "tri1" "triand" "trior" "trireg" "type" "typedef" "union"
+     "unique" "unsigned" "use" "uwire" "var" "vectored" "virtual" "void"
+     "wait" "wait_order" "wand" "weak0" "weak1" "while" "wildcard"
+     "wire" "with" "within" "wor" "xnor" "xor"
+     ;; 1800-2009
+     "accept_on" "checker" "endchecker" "eventually" "global" "implies"
+     "let" "nexttime" "reject_on" "restrict" "s_always" "s_eventually"
+     "s_nexttime" "s_until" "s_until_with" "strong" "sync_accept_on"
+     "sync_reject_on" "unique0" "until" "until_with" "untyped" "weak"
+     ;; 1800-2012
+     "implements" "interconnect" "nettype" "soft"
+     ;; AMS
+     "connectmodule" "endconnectmodule"
+     )
+    ))
 
 (defun acm-citre-clear-prefix-keyword ()
   (setq-local acm-citre-prefix-keyword ""))
+
+(defun acm-backend-citre-get-mode-keyword ()
+  (let* ((mode-keyword (list)))
+    (catch 'break
+      (dolist (item acm-keywords-alist)
+        (when (eq major-mode (car item))
+          (setq mode-keyword (seq-subseq item 1))
+          (throw 'break t))))
+      (when mode-keyword
+        (dolist (mk mode-keyword)
+          (setq mk (citre-put-property mk 'annotation "keyword"))))
+      mode-keyword))
 
 (defun acm-backend-citre-candidates (keyword)
   (when acm-enable-citre
@@ -111,6 +181,8 @@
           (setq-local acm-citre-search-keyword keyword)))
       (let* ((collection (unless (string-empty-p acm-citre-search-keyword)
                            (delete-dups (citre-capf--get-collection acm-citre-search-keyword)))))
+        (when acm-enable-keyword-complete
+          (setq collection (append (acm-backend-citre-get-mode-keyword) collection)))
         (when collection
           (dolist (candidate collection)
             (when (acm-candidate-fuzzy-search acm-citre-search-keyword candidate)
