320687a06e8b6aab2d1e7a359bb2c52f5e8cca77
Andy Stewart <lazycat.manatee@gmail.com>
2022-10-14 00:10:45 +0800

    Update userlist when first switch to telega buffer.

--- acm/acm-backend-telega.el
+++ acm/acm-backend-telega.el
@@ -22,30 +22,40 @@
 (defun acm-backend-telega-update-items ()
   "Update the optional items."
   ;; Scoped by current line: the @ character must be present before the cursor
-  (setq-local acm-backend-telega-items
-              (when (save-excursion (search-backward "@" (line-beginning-position) t))
-                (let ((at (char-to-string (char-before))))
-                  ;; Get data only when you enter @, otherwise return the acquired data directly
-                  (if (string= at "@")
-                    (mapcar (lambda (user)
-                              (let ((username (plist-get user :username))
-                                    (firstname (plist-get user :first_name)))
-                                (list :key username
-                                      :icon "at"
-                                      :label username
-                                      :display-label username
-                                      :annotation firstname
-                                      :backend "telega")))
-                            (acm-backend-telega-fetch-members))
-		    acm-backend-telega-items)))))
+  (when (and acm-enable-telega (eq major-mode 'telega-chat-mode))
+    (unless acm-backend-telega-items
+      (message "Fetch telega userlist...")
+      (setq-local acm-backend-telega-items
+                  (mapcar (lambda (user)
+                            (let ((username (plist-get user :username))
+                                  (firstname (plist-get user :first_name)))
+                              (list :key username
+                                    :icon "at"
+                                    :label username
+                                    :display-label username
+                                    :annotation firstname
+                                    :backend "telega")))
+                          (acm-backend-telega-fetch-members)))
+      (message "Fetch telega userlist done."))))
 
 (defun acm-backend-telega-candidates (keyword)
-  (when (and acm-enable-telega (eq major-mode 'telega-chat-mode))
-    (acm-backend-telega-update-items)
-    (acm-candidate-sort-by-prefix keyword acm-backend-telega-items)))
+  (when (and acm-enable-telega
+             (eq major-mode 'telega-chat-mode)
+             (save-excursion
+               (backward-char (length keyword))
+               (= (char-before) ?@)
+               ))
+    (if (string-equal keyword "")
+        acm-backend-telega-items
+      (let ((candiates (list)))
+        (dolist (item acm-backend-telega-items)
+          (when (or (string-match keyword (plist-get item :label))
+                    (string-match keyword (plist-get item :annotation)))
+            (add-to-list 'candiates item)))
+        (acm-candidate-sort-by-prefix keyword candiates)))))
 
-(defun acm-backend-telega-clean ()
-  (setq-local acm-backend-telega-items nil))
+;; Update userlist when first switch to telega buffer.
+(add-hook 'buffer-list-update-hook #'acm-backend-telega-update-items)
 
 (provide 'acm-backend-telega)
 ;;; acm-backend-telega.el ends here
