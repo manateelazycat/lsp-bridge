1619a203e025d702c4275130934f3c1d25da8fec
Jordan Anthony King <jordan@jordanking.dev>
2022-11-29 22:05:29 +0000

    Add omnisharp-dotnet to installer

--- lsp-bridge-lsp-installer.el
+++ lsp-bridge-lsp-installer.el
@@ -81,14 +81,18 @@
 
 ;;; Require
 (require 'cl-lib)
-
 ;;; Code:
 
 (defun lsp-bridge-install-omnisharp ()
   (interactive)
-  (let ((url "https://github.com/OmniSharp/omnisharp-roslyn/releases/latest/download/omnisharp-mono.zip")
+  (let* ((to-append (if (string= lsp-bridge-csharp-lsp-server "omnisharp-dotnet")
+			(if (eq system-type 'windows-nt)
+			    "omnisharp-win-x64-net6.0.zip"
+			  "omnisharp-linux-x64-net6.0.zip")
+		      "omnisharp-mono.zip"))
+	(url (concat "https://github.com/OmniSharp/omnisharp-roslyn/releases/latest/download/" to-append))
         (down-des (if (eq system-type 'windows-nt)
-                       (substitute-in-file-name "\\$USERPROFILE\\AppData\\Local\\Temp\\omnisharp-mono.zip")
+                       (substitute-in-file-name (concat "\\$USERPROFILE\\AppData\\Local\\Temp\\" to-append))
                     "/tmp/omnisharp-mono.zip"))
         (install-des (if (eq system-type 'windows-nt)
                          (expand-file-name (format "%s.cache/omnisharp/" user-emacs-directory))
