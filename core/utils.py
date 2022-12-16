db1995a87f44747579174e48e465b48ade284044
Andy Stewart <lazycat.manatee@gmail.com>
2022-12-16 11:46:37 +0800

    Revert old python patch.

--- core/utils.py
+++ core/utils.py
@@ -147,18 +147,10 @@ def get_emacs_func_result(method_name, *args):
 
 def get_command_result(command_string, cwd):
     import subprocess
-    import platform
-    from packaging import version
     
-    current_version = platform.python_version()
-    if version.parse(current_version) < version.parse("3.10.8"):
-        process = subprocess.Popen(command_string, cwd=cwd, shell=True, 
-                                   stdout=subprocess.PIPE, stderr=subprocess.PIPE,
-                                   encoding="utf-8")
-    else:
-        process = subprocess.Popen(command_string, cwd=cwd, shell=True, text=True,
-                                   stdout=subprocess.PIPE, stderr=subprocess.PIPE,
-                                   encoding="utf-8")
+    process = subprocess.Popen(command_string, cwd=cwd, shell=True, text=True,
+                               stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+                               encoding="utf-8")
     ret = process.wait()
     return "".join((process.stdout if ret == 0 else process.stderr).readlines()).strip()    # type: ignore
 
