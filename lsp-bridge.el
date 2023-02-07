9b999b90d55a2ac7a3418bd7f76e13f7d940d796
Andy Stewart <lazycat.manatee@gmail.com>
2023-02-07 17:34:15 +0800

    Use pyright-background-analysis as default python lsp server.

--- lsp-bridge.el
+++ lsp-bridge.el
@@ -358,7 +358,7 @@ Then LSP-Bridge will start by gdb, please send new issue with `*lsp-bridge*' buf
   "Default LSP server for Python language, you can choose `pyright', `jedi', `python-ms', `pylsp'."
   :type 'string)
 
-(defcustom lsp-bridge-python-ruff-lsp-server "pyright_ruff"
+(defcustom lsp-bridge-python-ruff-lsp-server "pyright-background-analysis_ruff"
   "Default LSP server for Python Ruff, you can choose `pyright_ruff', `jedi_ruff', `python-ms_ruff', `pylsp_ruff'."
   :type 'string)
 
