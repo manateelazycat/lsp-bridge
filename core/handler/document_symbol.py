7974fd1255970c2a1daf6f8ff37faeae1490a446
Andy Stewart <lazycat.manatee@gmail.com>
2023-11-11 17:38:37 +0800

    Fix issue #772 , swifty lsp server return different json struct with other lsp server.

--- core/handler/document_symbol.py
+++ core/handler/document_symbol.py
@@ -13,15 +13,22 @@ class DocumentSymbol(Handler):
         return dict()
 
     def process_response(self, response: dict) -> None:
-        symbols = []
-        current_defun = ''
-        for symbol in response:
-            range = symbol['location']["range"]
-            if range['start']['line'] < self.position['line'] < range['end']['line'] or\
-               (range['start']['line'] == self.position['line'] and range['start']['character'] <= self.position['character']) or\
-               (range['end']['line'] == self.position['line'] and range['end']['character'] >= self.position['character']):
-                if len(current_defun):
-                    current_defun += '.'
-                current_defun += symbol['name']
-                symbols.append(symbol)
-        eval_in_emacs("lsp-bridge-symbols--record-current-defun", current_defun)
+        try:
+            symbols = []
+            current_defun = ''
+            for symbol in response:
+                if "range" in symbol:
+                    range = symbol["range"]
+                else:
+                    range = symbol['location']["range"]
+                if range['start']['line'] < self.position['line'] < range['end']['line'] or\
+                   (range['start']['line'] == self.position['line'] and range['start']['character'] <= self.position['character']) or\
+                   (range['end']['line'] == self.position['line'] and range['end']['character'] >= self.position['character']):
+                    if len(current_defun):
+                        current_defun += '.'
+                    current_defun += symbol['name']
+                    symbols.append(symbol)
+            eval_in_emacs("lsp-bridge-symbols--record-current-defun", current_defun)
+        except:
+            import traceback
+            logger.error(traceback.format_exc())
