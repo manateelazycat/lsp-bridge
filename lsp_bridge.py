1876a9d3ec6cd2c31cfe22a3330926b88ef79490
Andy Stewart <lazycat.manatee@gmail.com>
2024-04-21 12:33:09 +0800

    Add support-single-file: false in tailwindcss.json

--- lsp_bridge.py
+++ lsp_bridge.py
@@ -641,7 +641,7 @@ class LspBridge:
             lang_server_info["support-single-file"] is False):
             self.turn_off(
                 filepath,
-                "ERROR: {} not support single-file, you need put this file in a git repository".format(single_lang_server))
+                "ERROR: {} not support single-file, you need put this file in a git repository or put .dir-locals.el in project root directory".format(single_lang_server))
 
             return False
 
