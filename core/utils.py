ca6ad8d3505a7b871acbf1ca2160dac1b3455066
werhner <gao9426@hotmail.com>
2024-04-26 17:27:37 +0800

    support code action on tramp file

--- core/utils.py
+++ core/utils.py
@@ -153,6 +153,10 @@ def get_remote_connection_info():
     global remote_connection_info
     return remote_connection_info
 
+def local_path_to_tramp_path(path, tramp_method):
+    tramp_path = path.replace("file://", "file://" + tramp_method)
+    return tramp_path
+
 def eval_in_emacs(method_name, *args):
     global lsp_bridge_server
 
