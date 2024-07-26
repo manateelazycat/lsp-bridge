66abe55e5c92f439b35ff3c6f477e0aa218a6d37
nohz.afk <149959021+nohzafk@users.noreply.github.com>
2024-07-27 01:26:49 +0800

    handle lang_server is empty list

--- lsp_bridge.py
+++ lsp_bridge.py
@@ -990,7 +990,7 @@ def read_lang_server_info(lang_server_path):
 
 def load_single_server_info(lang_server):
     lang_server_info_path = ""
-    if os.path.exists(lang_server) and os.path.dirname(lang_server) != "":
+    if isinstance(lang_server, str) and os.path.exists(lang_server) and os.path.dirname(lang_server) != "":
         # If lang_server is real file path, we load the LSP server configuration from the user specified file.
         lang_server_info_path = lang_server
     else:
