6362ae4ede8ebfe3fbdef99007574b5dd86f0ee7
Andy Stewart <lazycat.manatee@gmail.com>
2024-09-22 20:46:46 +0800

    Don't send document uri when call rust_reload_workspace

--- core/handler/rust_reload_workspace.py
+++ core/handler/rust_reload_workspace.py
@@ -5,6 +5,7 @@ class RustReloadWorkspace(Handler):
     name = "rust_reload_workspace"
     method = "rust-analyzer/reloadWorkspace"
     cancel_on_change = False
+    send_document_uri = False
 
     def process_request(self) -> dict:
         return dict()
