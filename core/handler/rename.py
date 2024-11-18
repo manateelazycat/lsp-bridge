31d6dc47f2e43991b0513b47d49b36bfaafe6a56
akiyuki ishikawa <aki.y.ishikawa@gmail.com>
2024-11-18 19:52:05 +0900

    fix code_action over tramp

--- core/handler/rename.py
+++ core/handler/rename.py
@@ -16,14 +16,7 @@ class Rename(Handler):
 
         remote_connection_info = get_remote_connection_info()
         logger.info(response)
-        if remote_connection_info != "" :
-            changes = response["changes"]
-            new_changes = {}
-            for file in changes.keys():
-                tramp_file = local_path_to_tramp_path(file, remote_connection_info)
-                new_changes[tramp_file] = changes[file]
-            response["changes"] = new_changes
-
+        convert_workspace_edit_path_to_tramped_path(response, remote_connection_info)
         eval_in_emacs("lsp-bridge-workspace-apply-edit", response)
-        
+
         message_emacs("Rename done.")
