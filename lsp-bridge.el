5a2db48b929c20eb17471e3da7d76e69ac603edb
Yuki <Yuki61803@gmail.com>
2025-01-04 15:10:56 +0800

    feat(lsp-bridge.el): add additional extension floors

--- lsp-bridge.el
+++ lsp-bridge.el
@@ -1083,7 +1083,7 @@ So we build this macro to restore postion after code format."
 
 (defun lsp-bridge-get-lang-server-by-extension (filename extension-list)
   "Get lang server for file extension."
-  (when-let* ((file-extension (file-name-extension filename))
+  (when-let* ((file-extension (substring filename (+ (cl-position ?. filename) 1) (length filename)))
               (langserver-info (cl-find-if
                                 (lambda (pair)
                                   (let ((extension (car pair)))
