ca1a4008a1bfab5087233697f789db2ca0117f9c
Andy Stewart <lazycat.manatee@gmail.com>
2025-01-06 12:42:29 +0800

    Support popup documentation

--- lsp-bridge.el
+++ lsp-bridge.el
@@ -2302,6 +2302,7 @@ Then we need call `lsp-bridge--set-mark-ring-in-new-buffer' in new buffer after 
       (read-only-mode -1)
       (erase-buffer)
       (insert value)
+      (lsp-bridge-replace-html-entities)
       (setq-local truncate-lines nil)
       (acm-markdown-render-content))
 
